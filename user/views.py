7eab82879addf783f2022db31a18603929ffbc79
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2025-02-16 20:37:48 +0100

    solution

--- user/views.py
+++ user/views.py
@@ -4,15 +4,17 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, EmailAuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
+    permission_classes = ()
     serializer_class = UserSerializer
 
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = EmailAuthTokenSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
